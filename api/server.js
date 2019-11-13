fdc188a9e809c35d36435277fde1135c9bb75129
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-11-13 14:41:22 -0600

    server up and running

---
+++ api/server.js
@@ -0,0 +1,23 @@
+//imports and dependencies
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+// const authRouter = require('../auth/auth-router');
+// const usersRouter = require('../users/users-router');
+
+//server
+const server = express();
+
+//middleware
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+//server test
+server.get('/', (req, res) => {
+    res.send('testing');
+});
+
+//export
+module.exports = server;
---
+++ index.js
@@ -0,0 +1,6 @@
+//imports
+const server = require('./api/server');
+
+//port
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log(`**server is listening on port ${port}**`))
